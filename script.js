7b419d7621b4968a3da28fd40daf569cfad48cb8
MicMit19 <30875208+MicMit19@users.noreply.github.com>
2017-08-14 09:04:25 -1000

    Add files via upload
    Added first, nick, and last name

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function sayHello() {
   //I create a function called sayHello//
-  var userName = prompt("What is your name?");
-  //I create a variable called userName and ask the user something//
-  window.alert("Hello " + userName);
+  var userFirstName = prompt("What is your first name?");
+  //I create a variable called userFirstName and ask the user what its first name is//
+  var userNickName = prompt("What is your nick name?");
+  //I create a variable called userNickName and ask the user what its nickname is//
+  var userLastName = prompt("What is your last name?");
+  //I create a variable called userLastName and ask the user what its last name is//
+  window.alert("Hello " + " " + userFirstName + " " + userNickName + " " + userLastName);
   //I create a popup window telling the userName hello//
 }
