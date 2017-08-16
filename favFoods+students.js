3dfa5e6de5c426e5b7b3682ce290db87798da14a
MicMit19 <30875208+MicMit19@users.noreply.github.com>
2017-08-16 08:43:07 -1000

    Add files via upload
    Added a new javascript example :D

---
+++ favFoods+students.js
@@ -0,0 +1,6 @@
+var favFoods = ["bacon", "Lettuce", "Tomato"];
+//put fav foods, they are assigned a number from 0-2
+var students = ["Michael", "Raene", "Chuck", "Ryan"]
+//put students, they are assigned a number from 0-3
+window.alert(students[2] + " " + favFoods[0]);
+//action, should pop up as "Chuck bacon", pulls out Chuck, or 2 from students, and bacon, or 0 from favFoods
