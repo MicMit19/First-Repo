8790debdfd5d3bf526d07064aff7e6d2e778722f
MicMit19 <30875208+MicMit19@users.noreply.github.com>
2017-08-16 09:23:57 -1000

    Add files via upload

---
+++ multiply.js
@@ -0,0 +1,8 @@
+var numberOne = prompt("Please enter a number")
+//Ask user for a number
+var numberTwo = prompt("Please enter another number")
+//Ask user for another number
+var numberThree = (numberOne*numberTwo);
+//Take both numnbers and multiply them and save the output as another variable
+window.alert(numberThree);
+//Output the value to the screen using window popup
